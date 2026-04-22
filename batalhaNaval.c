0d4bcd715830fd6bc2a585e1f9632b5be29e87ac
babilunardi <barbylunardi57@gmail.com>
2026-04-22 16:38:50 -0300

    batalhinha navalzinha

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,31 +10,60 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int tabuleiro[10][10];
+    int navio = 3;
+                
+     printf("***Bem vindo ao Tabuleiro de batalha naval da babi****\n");     
+     printf("   "); 
+     
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+
+     //Cabeca tabuleiro
+    
+    for (int i = 0; i < 10; i++)
+     { 
+        for(int j = 0; j < 10; j++)
+        {
+            tabuleiro[i][j] = 0; 
+        }
+     }
+          
+     for(int i = 0; i < 10; i++) 
+        {        
+      printf("%c ", letras[i]);
+        }
+
+    //Navio 1
+        tabuleiro[2][5] = navio;
+        tabuleiro[2][6] = navio;
+        tabuleiro[2][7] = navio;
+
+    //Navio 2
+        tabuleiro[6][4] = navio;
+        tabuleiro[7][4] = navio;
+        tabuleiro[8][4] = navio;
+
+       printf("\n");
+
+
+    //Tabuleiro corpo
+                                    
+      for (int i = 0; i < 10; i++)
+     { 
+        printf("%2d ", i + 1);         
+
+        for(int j = 0; j < 10; j++)
+        {       
+            printf("%d ", tabuleiro[i][j]);
+
+        }
+        printf("\n");
+     }
+    
+               
+                
 
     return 0;
 }
