ea39490f9bc8d7d9df91600455c3b47d73a28f23
babilunardi <barbylunardi57@gmail.com>
2026-04-25 13:15:44 -0300

    batalhanavalhardcore

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,91 +1,163 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
-int main() {
-       
-    char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int tabuleiro[10][10];
-    int navio = 3;
-    
-                
-     printf("***Bem vindo ao Tabuleiro de batalha naval da babi****\n");     
-     printf("   "); 
-     
+#define LINHAS 10
+#define COLUNA 10
+#define LINHAS2 3
+#define COLUNA2 5
+#define COLUNA3 3
 
 
-     //Cabeca tabuleiro
-    
-    for (int i = 0; i < 10; i++)
-     { 
-        for(int j = 0; j < 10; j++)
+
+int main (){ 
+
+char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+int tabuleiro[LINHAS][COLUNA];
+int navio = 3;
+int cruz[LINHAS2][COLUNA2];
+int cone[LINHAS2][COLUNA2];
+int octa[LINHAS2][COLUNA3];
+int origemcruz = 2, origemcruz2 = 2;
+int origemcone = 4, origemcone2 = 5;
+int origemocta = 4, origemocta2 = 0;
+
+        printf("***Tabuleiro de Batalha Naval da Babi***\n");
+        printf("   ");
+
+
+        //cabeca tabuleiro
+
+
+        for (int i = 0; i < LINHAS; i++)
+        { 
+        for(int j = 0; j < COLUNA; j++)
         {
             tabuleiro[i][j] = 0; 
         }
-     }
+        }
           
-     for(int i = 0; i < 10; i++) 
+        for(int i = 0; i < LINHAS; i++) 
         {        
-      printf("%c ", letras[i]);
+        printf("%c ", letras[i]);
         }
 
-        //diagonal 1
+        printf("\n");
 
 
-        for (int i = 6; i < 10; i++)
-      {
-        for (int j = 7; j < 10; j++)
+        // Cruz  
+
+        for (int i = 0; i < LINHAS2; i++)
         {
-            if (i == j) {tabuleiro[i][j] = 3;}
-
+        for (int j = 0; j < COLUNA2; j++)
+        {   cruz[i][j] = 0;
+            if (j == 2) {cruz[i][j] = 5;}
+            if (i == 1) {cruz[i][j] = 5;}
         }
-    }
-        //diagonal 2
-
-        for (int i = 0; i < 10; i++)
-      {
-        for (int j = 7; j < 10; j++)
+        }      
+        
+        for (int i = 0; i < LINHAS2; i++)
         {
-            if (i + j == 9) {tabuleiro[i][j] = 3;}
-
+        for (int j = 0; j < COLUNA2; j++)
+        {
+        tabuleiro[i + origemcruz][j + origemcruz2] = cruz[i][j];
+        cruz[i][j] = 0;
         }
-    }
-      
+        }   
 
-    //Navio 1
-        tabuleiro[4][5] = navio;
-        tabuleiro[4][6] = navio;
-        tabuleiro[4][7] = navio;
 
-    //Navio 2
+        //Cone
+
+        for (int i = 0; i < LINHAS2; i++)
+        {
+        for (int j = 0; j < COLUNA2; j++)
+        {   cone[i][j] = 0;
+
+            if (i == 0 && j == 2) {cone[i][j] = 5;}
+            if (i == 1 && (j == 1 || j == 2 || j == 3)) {cone[i][j] = 5;}
+            if (i == 2) {cone[i][j] = 5;}
+        }
+        }   
+
+        for (int i = 0; i < LINHAS2; i++)
+        {
+        for (int j = 0; j < COLUNA2; j++)
+        {
+        tabuleiro[i + origemcone][j + origemcone2] = cone[i][j];
+        cone[i][j] = 0;
+        }
+        } 
+
+
+        //Octaedro
+
+        for (int i = 0; i < LINHAS2; i++)
+        {
+        for (int j = 0; j < COLUNA3; j++)
+        {   octa[i][j] = 0;
+
+            if (j == 1) {octa[i][j] = 5;}
+            if (i == 1) {octa[i][j] = 5;}
+        }
+        }      
+
+        for (int i = 0; i < LINHAS2; i++)
+        {
+        for (int j = 0; j < COLUNA3; j++)        
+        {
+            tabuleiro[i + origemocta][j + origemocta2] = octa[i][j];
+            octa[i][j] = 0;
+        }
+        } 
+
+        //Navio 1
+        tabuleiro[0][5] = navio;
+        tabuleiro[0][6] = navio;
+        tabuleiro[0][7] = navio;
+
+        //Navio 2
         tabuleiro[6][4] = navio;
         tabuleiro[7][4] = navio;
         tabuleiro[8][4] = navio;
 
-       printf("\n");
 
-     
+        // Navio 3 - Diagonal 1
 
-    //Tabuleiro corpo
-                                    
-      for (int i = 0; i < 10; i++)
-     { 
-        printf("%2d ", i + 1);         
 
-        for(int j = 0; j < 10; j++)
+        for (int i = 1; i < LINHAS; i++)
+        {
+        for (int j = 7; j < COLUNA; j++)
+        {
+            if (i == j) {tabuleiro[i][j] = 3;}
+
+        }
+        }
+
+        // Navio 4 em Diagonal 2
+
+
+        for (int i = 7; i < 10; i++)
+        {
+        for (int j = 0; j < 10; j++)
+        {
+            if (i + j == 9) {tabuleiro[i][j] = 3;}
+
+        }
+        }
+
+        for (int i = 0; i < LINHAS; i++)
+        { 
+        printf("%2d ", i + 1);
+        
+        for(int j = 0; j < COLUNA; j++)
         {       
             printf("%d ", tabuleiro[i][j]);
 
         }
         printf("\n");
-     }
+        }    
 
+        return 0;
 
-
-
-     return 0;
+    }
 
     }
     
